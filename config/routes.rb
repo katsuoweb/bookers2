3e4bd18f0888b4177a931f5130cd883e5cb1af9f
katsuoweb <massadub@gmail.com>
2020-12-31 17:54:48 +0000

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :users, only: [:show]
   resources :books, only: [:new, :create, :index, :show, :destroy]
 end
