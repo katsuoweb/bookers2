cd37a27f604d9a03ca06a5a5493e6fda5349c70d
katsuoweb <massadub@gmail.com>
2021-01-01 15:49:38 +0000

    Add users_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @books = Book.where(user_id: @user.id)
