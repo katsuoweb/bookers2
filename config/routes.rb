684bff333f03600715084fd92af77e6595e08540
katsuoweb <massadub@gmail.com>
2021-01-02 10:39:46 +0000

    Add user edit form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   get 'users/:id/edit' => 'users#edit'
+  patch 'users/:id' => 'users#update'
   resources :users, only: [:index, :show]
   resources :books, only: [:new, :create, :index, :show, :destroy]
 end
