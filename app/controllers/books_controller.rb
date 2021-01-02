c2e9509830d320124b40757ff921107a8ce04fee
katsuoweb <massadub@gmail.com>
2021-01-02 15:22:03 +0000

    Add book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,27 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def index
     @user = User.find(current_user.id)
     @books = Book.all
+    @new_book = Book.new
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
 end
