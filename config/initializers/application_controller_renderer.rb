d108b95bc4b966bf7dc8b53bc929f5eb17513e28
katsuoweb <massadub@gmail.com>
2021-01-02 20:50:59 +0000

    Add user image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8a7c9650e47fbd92d400a468078d3209f45b3dc286ad6fe06c9865a4ce06f8b7f7f9305fd8da8c1699a70ec816fd6efae69a3826118c197890ddb43a2cee658d'
