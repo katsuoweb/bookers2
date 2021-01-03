441cdf996a082c15eecb990e5a50afce90f15d02
katsuoweb <massadub@gmail.com>
2021-01-03 09:52:03 +0000

    Add flash and change path after sign-in and sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
+
+  private
+
+  def after_sign_in_path_for(resources_or_scope)
+    user_path(current_user.id)
+  end
+
+  def after_sign_up_path_for(resources_or_scope)
+    user_path(current_user.id)
+  end
 end
